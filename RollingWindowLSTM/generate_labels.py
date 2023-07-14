fa989dcbb4c6acd64565bc9d3cff2832de24d527
Gordon Doore <gcdoor25@colby.edu>
2023-07-13 22:19:26 -0400

    reshape medians, allow for pass in other reshape optionally, still trying to figure out how to confirm correctness of labels

--- RollingWindowLSTM/generate_labels.py
+++ RollingWindowLSTM/generate_labels.py
@@ -5,111 +5,175 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-# Retrieve S&P 500 constituents
-url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-table = soup.find('table', {'class': 'wikitable sortable'})
+def download_smp_csv(tickers, start_date, end_date, dest):
+    """
+    Downloads stock price data for the given tickers, from start_date to end_date. It is part of the data collection step.
+    :param tickers: A list of company tickers.
+    :param start_date: A string representing the start date in 'YYYY-MM-DD' format. For example, '1991-01-01'.
+    :param end_date: A string representing the end date in 'YYYY-MM-DD' format. For example, '2023-07-01'.
+    :return: A pandas DataFrame of 'Adjusted Close' prices of the stocks, or None if data download fails.
+    """
+    tickers_string = ' '.join(tickers)
+    try:
+        all_data = yf.download(tickers_string, start=start_date, end=end_date)
+    except Exception as e:
+        print(f"Failed to download data: {str(e)}")
 
-tickers = []
-for row in table.find_all('tr')[1:]:
-    ticker = row.find_all('td')[0].text.strip()
-    tickers.append(ticker)
+    # save all data to csv
+    all_data['Adj Close'].to_csv(dest)
 
-# Join all tickers into a single string with spaces in between
+    
+def get_sp500_tickers():
+    """
+    Retrieves the list of S&P 500 companies from the corresponding Wikipedia page. This list is used to download stock data.
+    :return: A list of tickers of all companies listed on the S&P 500.
+    """
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table', {'class': 'wikitable sortable'})
 
-tickers_string = ' '.join(tickers)
+    tickers = []
+    for row in table.find_all('tr')[1:]:
+        ticker = row.find_all('td')[0].text.strip()
+        tickers.append(ticker)
+    response.close()
+    return tickers
 
-# Define the study period parameters
-study_period_length = 1000
-training_period = 750
-sequence_length = 240
-stride = 250  # Rolling forward by 250 days
+def get_data(tickers, start_date, end_date):
+    """
+    Downloads stock price data for the given tickers, from start_date to end_date. It is part of the data collection step.
+    :param tickers: A list of company tickers.
+    :param start_date: A string representing the start date in 'YYYY-MM-DD' format. For example, '1991-01-01'.
+    :param end_date: A string representing the end date in 'YYYY-MM-DD' format. For example, '2023-07-01'.
+    :return: A pandas DataFrame of 'Adjusted Close' prices of the stocks, or None if data download fails.
+    """
+    tickers_string = ' '.join(tickers)
+    try:
+        all_data = yf.download(tickers_string, start=start_date, end=end_date)
+    except Exception as e:
+        print(f"Failed to download data: {str(e)}")
+        return None
+    return all_data['Adj Close']
 
-# Retrieve historical data for all tickers at once since 1991
-start_date = '1991-01-01'
-end_date = '2023-07-01'
+def create_simple_returns(all_data, tickers):
+    """
+    Computes the simple returns for each stock and calculates the cross-sectional median return. This transformation is required to feed the data to the model.
+    :param all_data: A pandas DataFrame containing 'Adjusted Close' prices of the stocks.
+    :param tickers: A list of company tickers.
+    :return: A pandas DataFrame of simple returns and cross-sectional median for each day.
+    """
+    with_simple_returns = pd.DataFrame({ticker: all_data[ticker].pct_change() for ticker in tickers})
+    with_simple_returns['Cross-sectional Median'] = with_simple_returns.median(axis=1)
+    return with_simple_returns.tail(-1)
 
-try:
-    all_data = yf.download(tickers_string, start=start_date, end=end_date)
-except Exception as e:
-    print(f"Failed to download data: {str(e)}")
-response.close()
-# Extract 'Adj Close' columns
-print(all_data.head())
-all_data = all_data['Adj Close']
-print(all_data.head())
+def create_study_periods(with_simple_returns, study_period_length, sequence_length, stride, include_median = True):
+    """
+    Generates study periods and labels for each period. Each study period is a sequence of historical returns and the label indicates if the return is below or above the median.
+    :param with_simple_returns: A pandas DataFrame of simple returns and cross-sectional median.
+    :param study_period_length: An integer specifying the length of a study period, such as 1000 days.
+    :param sequence_length: An integer specifying the sequence length within each study period, such as 240 days.
+    :param stride: An integer specifying the number of days to roll forward, such as 250 days.
+    :param include_median: A boolean specifying whether to include cross-sectional median.
+    :return: A tuple of lists (study_periods, labels) if include_median=False, or (study_periods, labels, median_arr) if include_median=True.
+    """
+    study_periods = []
+    labels = []
+    median_arr = []
 
-# Create a new DataFrame to store simple returns with single-level columns
-with_simple_returns = pd.DataFrame({ticker: all_data[ticker].pct_change() for ticker in tickers})
+    for i in tqdm(range(0, len(with_simple_returns) - study_period_length + 1, stride), desc="Generating Study Periods"):
+        period_data = with_simple_returns.iloc[i:i + study_period_length].copy()
+        period_data = period_data.dropna(axis=1)
+        training_data = period_data
+        median_to_label = period_data['Cross-sectional Median'].values
+        label = np.where(training_data.values < median_to_label[:, np.newaxis], 0, 1)
+        sequences = [training_data.values[j:j + sequence_length] for j in range(0, len(training_data) - sequence_length-1, 1)]
+        label = [label[1+j + sequence_length] for j in range(0, len(training_data) - sequence_length-1, 1)]
+        if include_median:
+            median_formatted = [median_to_label[1+j + sequence_length] for j in range(0, len(training_data) - sequence_length-1, 1)]
+            median_formatted = np.array(median_formatted)
+            median_arr.append(median_formatted)
+            
+        label = np.array(label)
+        study_periods.append(sequences)
+        labels.append(label)
+    if include_median:
+        return study_periods, labels, median_arr
+    else:
+        return study_periods, labels
 
-# Calculate the cross-sectional median
-with_simple_returns['Cross-sectional Median'] = with_simple_returns.median(axis=1)
-with_simple_returns = with_simple_returns.tail(-1)
-print(with_simple_returns.head())
+def reshape_arrays(study_periods, labels, medians = None):
+    """
+    Reshapes the study periods and labels into the format required for training a machine learning model. This step is necessary to match the input shape requirements of many machine learning models.
+    :param study_periods: A list of study periods.
+    :param labels: A list of labels corresponding to each study period.
+    :return: A tuple of numpy arrays (reshaped study periods, reshaped labels).
+    """
+    study_periods_array = np.concatenate(
+        [np.array(array) for array in tqdm(study_periods, desc="Study Periods") if array is not None],
+        axis=2
+    )
 
-# Process each study period
-study_periods = []
-labels = []
+    labels_array = np.concatenate(
+        [np.array(array) for array in tqdm(labels, desc="Labels") if array is not None],
+        axis=1
+    )
 
-for i in tqdm(range(0, len(with_simple_returns) - study_period_length + 1, stride), desc="Generating Study Periods"):
-    period_data = with_simple_returns.iloc[i:i + study_period_length].copy()
+    if medians is not None: 
+        medians_array = np.concatenate(
+            [np.array(array) for array in tqdm(medians, desc="Medians") if array is not None],
+            axis=0
+        )
+        medians_array = np.array(medians_array)
+        medians_array = np.reshape(medians_array, (medians_array.shape[0], 1))
+    
 
-    # Drop columns with null values
-    period_data = period_data.dropna(axis=1)
+    if len(study_periods_array.shape) >= 3:
+        study_periods_array = study_periods_array.swapaxes(0, 2)
+        study_periods_array = study_periods_array.swapaxes(1, 2)
+    else:
+        print("Study periods array has less than three dimensions. Cannot swap axes.")
 
-    # Create overlapping sequences for the training period
-    training_data = period_data
-    median_to_label = period_data['Cross-sectional Median'].values
-    #We want to label each sequnce according to the true value of the day after the sequence end
-    label = np.where(training_data.values < median_to_label[:, np.newaxis], 0, 1)
-    #this should be all labels in a given study period
-    #so now we want to index to 240, and then assign label by walking from there
+    labels_array = np.array(labels_array)
+    labels_array = labels_array.swapaxes(0,1)
+    labels_array = np.reshape(labels_array, (-1, 1))
+    study_periods_array = np.reshape(study_periods_array, (-1, 240, 1))
+    if medians is not None:
+        return study_periods_array, labels_array, medians_array
 
-    sequences = [training_data.values[j:j + sequence_length] for j in range(0, len(training_data) - sequence_length-1, 1)]
-    label = [label[1+j + sequence_length] for j in range(0, len(training_data) - sequence_length-1, 1)]
-    label = np.array(label)
-    print(label.shape)
+    return study_periods_array, labels_array
 
-    # Add to the study periods list
-    study_periods.append(sequences)
-    labels.append(label)
+def save_data(study_periods_array, labels_array):
+    """
+    Saves the study periods and labels to numpy files. This allows the data to be loaded later for analysis or model training.
+    :param study_periods_array: A numpy array of reshaped study periods.
+    :param labels_array: A numpy array of reshaped labels.
+    """
+    np.save("data/test-sp500-simple-return-periodized.npy", study_periods_array)
+    np.save("data/test-sp500-simple-return-labels.npy", labels_array)
 
-#loop through study period and we want to reshape along the 0th axis
-study_periods_array = None
-labels_array = None
+def main():
+    """
+    The main function of the program. Coordinates all the steps: downloading stock price data, calculating returns, generating study periods and labels, reshaping the arrays, and saving them to disk. It sets specific values for the study period length (1000), sequence length (240), and stride (250). It also specifies a fixed list of tickers ['AAPL', 'MMM', 'AMZN', 'MSFT', 'TSLA'] for demonstration purposes.
+    """
+    study_period_length = 1000
+    sequence_length = 240
+    stride = 250  # Rolling forward by 250 days
+    #load data from csv
+    all_data = pd.read_csv('data/sp500-all-data.csv', index_col=0)
+    #grab tickers from column names
+    tickers = all_data.columns.values
+    with_simple_returns = create_simple_returns(all_data, tickers)
+    study_periods, labels, crossec_meds = create_study_periods(with_simple_returns, study_period_length, sequence_length, stride, True)
+    
+    study_periods_array, labels_array, medians_array = reshape_arrays(study_periods, labels, crossec_meds)
+    print(study_periods_array.shape)
+    print(labels_array.shape)
+    print(medians_array.shape)
+    #save_data(study_periods_array, medians)
 
-study_periods_array = np.concatenate(
-    [np.array(array) for array in tqdm(study_periods, desc="Study Periods") if array is not None],
-    axis=2
-)
+if __name__ == "__main__":
+    main()
+   
 
-labels_array = np.concatenate(
-    [np.array(array) for array in tqdm(labels, desc="Labels") if array is not None],
-    axis=1
-)
 
-print(study_periods_array.shape)
-
-labels_array = labels_array.reshape(labels_array.shape[0], labels_array.shape[1], 1)
-# Check if there are at least three dimensions before attempting to swap axes
-if len(study_periods_array.shape) >= 3:
-    study_periods_array = study_periods_array.swapaxes(0, 2)
-    study_periods_array = study_periods_array.swapaxes(1, 2)
-else:
-    print("Study periods array has less than three dimensions. Cannot swap axes.")
-    # Add some error handling or debugging here if needed
-
-medians = np.array(labels_array)
-medians =medians.swapaxes(0,1)
-
-medians = np.reshape(medians, (-1, 1))
-study_periods_array = np.reshape(study_periods_array, (-1, 240, 1))
-
-# Now we want to stack the last dimension into the array along the 0th axis
-print(medians.shape)
-print(study_periods_array.shape)
-# Save the formatted data and labels
-
-np.save("data/test-sp500-simple-return-periodized.npy", study_periods_array)
-np.save("data/test-sp500-simple-return-labels.npy", medians)
